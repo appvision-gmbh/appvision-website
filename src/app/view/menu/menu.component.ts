4a2dbbae270e7630b1750d1710b22f565e229b9b
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2021-03-13 01:36:56 +0100

    feat: add transparent menu when scrolled up

--- src/app/view/menu/menu.component.ts
+++ src/app/view/menu/menu.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-    selector: "app-menu",
-    templateUrl: "./menu.component.html",
-    styleUrls: ["./menu.component.scss"],
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
 
-    collapsed: boolean = true;
+    readonly SCROLLED_TO_TOP_THRESHOLD = 200;
+
+    collapsed = true;
+    scrolledToTop = true;
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll = ($event: Event): void => {
+        const pos: number = (document.documentElement.scrollTop || document.body.scrollTop);
+        this.scrolledToTop = pos <= this.SCROLLED_TO_TOP_THRESHOLD;
+    }
 
     constructor() { }
 
