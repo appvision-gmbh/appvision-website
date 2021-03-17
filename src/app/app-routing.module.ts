38ce0ed022cf846eb8bb7b20912c333d398b30ec
Andreas Aeschlimann <andreas.aeschlimann@appvision.ch>
2021-03-17 01:59:56 +0100

    chore: rename url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ServicesComponent } from './view/pages/services/services.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'products', component: ProductsComponent},
+    {path: 'safelog', component: ProductsComponent},
     {path: 'company', component: CompanyComponent},
     {path: 'services', component: ServicesComponent},
     {path: '**', redirectTo: ''},
